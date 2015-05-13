502e1100dad9d81c088ca60159073d8f9f7220d2
marcoberri@gmail.com <marcoberri@gmail.com>
2015-05-13 12:45:41 +0200

    fix log path

--- app/conf.js
+++ app/conf.js
@@ -1,5 +1,5 @@
 module.exports = {
-          "ACCESS_LOG":'/opt/logs/nodejs-access.log',
+          "ACCESS_LOG":'/home/marco/meteo/logs/nodejs-access.log',
           "APP_PORT": 3001,
           "APP_URLPROXY":"http://localhost:3001",
           "MONGO_URL": "@localhost:27017/mbmeteolacrosse"
